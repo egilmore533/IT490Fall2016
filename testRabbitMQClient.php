85b0c41fd66bbea89b1fe1919af5d3b6a2f45a8f
Stephen <sl428@njit.edu>
2016-10-11 10:26:00 -0400

    mysql with register and sha

--- testRabbitMQClient.php
+++ testRabbitMQClient.php
@@ -15,9 +15,9 @@ else
 }
 
 $request = array();
-$request['type'] = "Login";
-$request['username'] = "steve";
-$request['password'] = "password";
+$request['type'] = "register";
+$request['username'] = "ShaEricGo";
+$request['password'] = "psswrd14";
 $request['message'] = $msg;
 $response = $client->send_request($request);
 //$response = $client->publish($request);
