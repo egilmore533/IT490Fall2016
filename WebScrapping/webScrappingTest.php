63a5598e2658ee45e080178238feabe4f040fc37
Eric <epg5@njit.edu>
2016-10-15 11:54:43 -0400

    added code to get all trainers from a page with multiple trainer tables

--- WebScrapping/webScrappingTest.php
+++ WebScrapping/webScrappingTest.php
@@ -11,47 +11,58 @@ $trainer_table_regex = '/>Money(.+)<\/table>/sU';
 
 preg_match_all($trainer_table_regex,$data,$trainer_tables);
 
+
+$position = 0;
+$j = 0;
+foreach($trainer_tables[1] as $trainer_table)
+{
+
 $trainer_regex = '/<td (.+)<\/tr>/sU';
 
-preg_match_all($trainer_regex,$trainer_table[1],$trainers);
+preg_match_all($trainer_regex,$trainer_table,$trainers);
 
 $trainer_type_regex = '/Pokemon_FRLG_(.+).png/U';
 $trainer_types = array();
-$i = $position;
+$i = 0;
 foreach($trainers[0] as $trainer)
 {
 	preg_match($trainer_type_regex,$trainer,$trainer_types[$i]);
-	$final_array[$i][0] = $trainer_types[$i][1];
+	$final_array[$i+$position][0] = $trainer_types[$i][1];
 	$i++;
 }
 
 
-$trainer_name_regex = '/\/><\/a>([A-Z][a-z]+)<\/td>/';
+$trainer_name_regex = '/\/><\/a>( ?[A-Z][a-z]+)<\/td>/';
 $trainer_names = array();
-$i = $position;
+$i = 0;
 foreach($trainers[0] as $trainer)
 {
 	preg_match($trainer_name_regex,$trainer,$trainer_names[$i]);
-	$final_array[$i][1] = $trainer_names[$i][1];
+	$final_array[$i+$position][1] = $trainer_names[$i][1];
 	$i++;
 }
 
 
 $trainer_pokemon_regex = '/"bulbapedia:(.+) \(/U';
 $trainer_pokemons = array();
-$i = $position;
+$i = 0;
 $pokemon_num = 0;
 foreach($trainers[0] as $trainer)
 {
 	preg_match_all($trainer_pokemon_regex,$trainer,$trainer_pokemons[$i]);
 	foreach($trainer_pokemons[$i][1] as $pokemon)
 	{
-		$final_array[$i][$pokemon_num + 2] = $pokemon;
+		$final_array[$i+$position][$pokemon_num + 2] = $pokemon;
 		$pokemon_num++;
 	}
 	$pokemon_num = 0;
 	$i++;
 }
+
+$position = $i;
+$j++;
+
+}//end of loop for seperate trainer tables
 
 var_dump($final_array);
 
