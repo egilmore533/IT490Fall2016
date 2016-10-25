c4d71f782ea293522a29e13988bf1641f3a8da01
Stephen <sl428@njit.edu>
2016-10-25 12:04:15 -0400

    php fixing

--- MySqlConnection.php
+++ MySqlConnection.php
@@ -6,7 +6,6 @@ require_once('rabbitMQLib.inc');
 
 function doLogin($username,$password)
 {
-<<<<<<< HEAD
 	
 	///MySQL Connection
 	$servername = "localhost";
@@ -24,42 +23,6 @@ function doLogin($username,$password)
 		die("Connection failed: " . $conn->connect_error);
 	}
 	else
-=======
-
-///MySQL Connection
-$servername = "localhost";
-$DBuser = "it490";
-$DBpass = "whoGivesaFuck!490";
-$database = "userlogin";
-
-//Create Connection
-$conn = new mysqli($servername, $DBuser, $DBpass, $database);
-
-//Check Connection
-if($conn->connect_error){
-        die("Connection failed: " . $conn->connect_error);
-}
-
-else
-{
-$s = "SELECT * FROM userlogin";
-($result = mysqli_query($conn, $s)) or die (mysqli_error());
-        echo "Connected Successfully";
-}
-//MySQL Connection
-
-$checkDB = 0;
-
-// lookup username and password in database
-while($r=mysqli_fetch_array($result))
-{
-	$usernameDB = $r["username"];
-	$passwordDB = $r["password"];
-	var_dump($username);
-	var_dump($password);
-
-	if($username == $usernameDB && sha1($password) == $passwordDB)
->>>>>>> 738a043e885a297315cbfb7dde4e5dbb5d4047bb
 	{
 		$s = "SELECT * FROM userlogin";
 		($result = mysqli_query($conn, $s)) or die (mysqli_error());
@@ -74,7 +37,6 @@ while($r=mysqli_fetch_array($result))
 		($result2 = mysqli_query($conn2, $s)) or die (mysqli_error());
 		echo "Connected Successfully";
 	}
-<<<<<<< HEAD
 	//MySQL Connection
 	
 	$checkDB = 0;
@@ -128,15 +90,6 @@ while($r=mysqli_fetch_array($result))
 	        return array("success" => '0', 'message'=>"Server received request and processed");
 	//return false if not valid
 	
-=======
-}
-
-$conn->close();
-
-if($checkDB == 0)
-        return array("success" => '0', 'message'=>"Server received request and processed");
-                //return false if not valid
->>>>>>> 738a043e885a297315cbfb7dde4e5dbb5d4047bb
 }
 
 function register($username, $password)
@@ -160,7 +113,6 @@ function register($username, $password)
 		die("Connection failed: " . $conn->connect_error);
 	}
 	else
-<<<<<<< HEAD
 	{
 		$s = "SELECT * FROM userlogin";
 		($result = mysqli_query($conn, $s)) or die (mysqli_error());
@@ -176,13 +128,6 @@ function register($username, $password)
 		echo "Connected Successfully";
 	}
 	//MySQL Connection
-=======
-		$checkDB = 1;
-}
-
-if($checkDB == 1)
-{
->>>>>>> 738a043e885a297315cbfb7dde4e5dbb5d4047bb
 	
 	// 	lookup username and password in database
 	while($r=mysqli_fetch_array($result))
@@ -197,13 +142,7 @@ if($checkDB == 1)
 		
 	}
 	
-<<<<<<< HEAD
 	if($checkDB == 1)
-=======
-	//Non-Sha password insert
-	//$reg = "INSERT INTO userlogin (username, password) VALUES('$username', '$password')";
-	if($conn->query($reg) === TRUE)
->>>>>>> 738a043e885a297315cbfb7dde4e5dbb5d4047bb
 	{
 		
 		$sp = $password;
@@ -252,16 +191,8 @@ if($checkDB == 1)
 	else
 		return array("success" => '0', 'message'=>"Server received request and processed");
 	
-<<<<<<< HEAD
-=======
-	echo "Error: " . $reg . "<br>" . $conn->error;
-	var_dump($username);
-	var_dump($password);
-	}
->>>>>>> 738a043e885a297315cbfb7dde4e5dbb5d4047bb
 }
 
-<<<<<<< HEAD
 function addfunds($username, $add)
 {
 	//MySQL Connection
@@ -317,16 +248,7 @@ function addfunds($username, $add)
 			
 	}
 
-=======
-$conn->close();
-
-if($checkDB == 1)
-	return array("success" => true, 'message'=>"Server received request and processed");
-else
-	return array("success" => '0', 'message'=>"Server received request and processed");
->>>>>>> 738a043e885a297315cbfb7dde4e5dbb5d4047bb
 }
-
 function requestProcessor($request)
 {
 	echo "received request".PHP_EOL;
