2a5ac0d4fccd224269ddf808292c9cd77d2cac5d
Eric <epg5@njit.edu>
2016-11-17 13:08:13 -0500

    expanded deploy and bundle code

--- bundle/bundle.php
+++ bundle/bundle.php
@@ -1,16 +1,22 @@
 #!/usr/bin/php
 <?php
 
-include 'deployment_request.php';
+include 'requests.php';
 
 
-$name="test";
+function bundle($argv)
+{
+
+$ini_file=$argv[2];
+$ini_array=parse_ini_file($ini_file);
+
+$name=$ini_array['PACKAGE_NAME'];
 
 $request=array();
 $request['type']="version_number";
 $request['version_name']=$name;
 
-$response = make_request($request);
+$response = make_deployment_machine_request($request);
 
 $version=$response['version_number'];
 $package_name=$name.$version;
@@ -18,16 +24,49 @@ $directory="/var/packages/$package_name";
 
 shell_exec("mkdir $directory");
 
-//get file paths from config file
+$source_directory=$ini_array['SOURCE_DIRECTORY'];
+foreach($ini_array['FILES'] as $file)
+{
+	shell_exec("scp $source_directory$file $directory");
+}
 
-$source_directory="/var/packages/replace_tools.php";
-shell_exec("scp $source_directory $directory");
+shell_exec("scp $source_directory$ini_file $directory");
 
 shell_exec("tar -czvf $package_name.tar.gz -C $directory .");
 shell_exec("mv $package_name.tar.gz $directory");
 
-$ipaddress = "10.200.20.136";
-shell_exec("scp $directory.tar.gz steven@$ipaddress:$directory");
+$user=$ini_array['USER'];
+$pass=$ini_array['PASSWORD'];
+$ipaddress=$ini_array['IP_ADDRESS'];
+
+$bundle_request = array();
+$bundle_request['type'] = 'bundle';
+$bundle_request['version_name'] = "$package_name.tar.gz";
+
+$response = make_deployment_machine_request($bundle_request);
+
+}
+
+
+function deploy($argv)
+{
+	$request = array();
+	$request['type'] = "deploy_and_install";
+	$request['ip_address'] = $argv[2];
+	$request['name'] = $argv[3];
+	$request['version_number'] = $argv[4];
+	$request['file_name'] = "$argv[3]$argv[4].tar.gz";
+	$request['config'] = $argv[5];
+	var_dump($request);
+	var_dump(make_deployment_machine_request($request));
+}
+
+var_dump($argv);
+
+if($argv[1] == 'bundle')
+{	bundle($argv);	}
+if($argv[1] == 'deploy')
+{	 deploy($argv);	}
 
 ?>
 
