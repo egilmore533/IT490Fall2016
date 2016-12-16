73883743ae7123cb0705e211a1fcc68d087594c9
Eric <epg5@njit.edu>
2016-12-15 22:40:39 -0500

    fixed make teams to create a json file for the team

--- showdown_sim/make_team.php
+++ showdown_sim/make_team.php
@@ -14,42 +14,59 @@ function makeFile($filename, $textArray)
 	fclose($the_file);
 }
 
-function makeTrainerFile($trainerid)
+function makeTrainerFile($trainerid,$trainer_num)
 {
 	$textArray = array();
 	$trainer_select = "select * from info where trainerid=$trainerid";
 	$database = "Trainer";
 	$resultsTrainer = MySQLLib::makeDBSelection($trainer_select,$database);
-	$filename = "";
+
+	if($trainer_num == 1)
+		$filename="trainer1.json";
+	else
+		$filename="trainer2.json";
+
 	while($r=mysqli_fetch_array($resultsTrainer))
 	{
-		$filename = $r['name'].".txt";
 		$linenum = 0;
+        	$textArray[$linenum++] = "{\n";
+		$textArray[$linenum++] = "\"name\": \"".$r['name']."\",\n";
 		for($i = 1; $i < 7; $i++)
 		{
-			if($r["pokemon$i"] == "")
-				break;
+			$pokemonid = $r["pokemon$i"."id"];
 
-			$textArray[$linenum++] = $r["pokemon$i"]."\n";
-			$pokemon_select = "select * from pokemoves where pokename='".$r['pokemon'.$i]."'";
-                	$resultsPokemon = MySQLLib::makeDBSelection($pokemon_select,"Trainer");
-			while($r2=mysqli_fetch_array($resultsPokemon))
+			$pokemon_select = "select * from pokemon where pokemonid=$pokemonid";
+			$resultsPokemon = MySQLLib::makeDBSelection($pokemon_select,$database);
+
+			$r2=mysqli_fetch_array($resultsPokemon);
+
+			$textArray[$linenum++] = "\"pokemon$i\": \n";
+			$textArray[$linenum++] = " { \"name\": \"".$r2['name']."\",\n";
+			$textArray[$linenum++] = "   \"level\": \"".$r2['level']."\",\n";
+			$move_select = "select * from moves where pokemonid=$pokemonid";
+
+			$resultsMoves = MySQLLib::makeDBSelection($move_select,$database);
+
+			$r3=mysqli_fetch_array($resultsMoves);
+			for($j=1; $j < 5; $j++)
 			{
-				var_dump($r2);
-				for($j = 1; $j < 5; $j++)
+				$text = "   \"move$j\": \"".$r3["move$j"];
+				if($j + 1 >= 5)
 				{
-					$textArray[$linenum++] = "-".$r2["move$j"]."\n";	
+					if($i + 1 >= 7)
+						$textArray[$linenum++] = $text . "\" \n}\n";
+					else
+						$textArray[$linenum++] = $text . "\" \n},\n";
 				}
-				$textArray[$linenum++] = "\n";
-				break;
+				else
+					$textArray[$linenum++] = $text . "\", \n";
 			}
-			
 		}
-		
+		$textArray[$linenum++] = "}\n";	
 	}
 	makeFile($filename,$textArray);
 }
 
-makeTrainerFile(213);
+makeTrainerFile(366,1);
 
 ?>
